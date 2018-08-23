e6603bb6ffdfd05cbc5f4e67e5b90b08df39bef9
Bohdan <batievbt@gmail.com>
2018-08-23 16:10:57 +0000

    Custom validators

---
+++ src/Validator/Constraints/UniqueValues/AbstractUniqueValueValidator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Helpcrunch\Validator\Constraints\UniqueValues;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+abstract class AbstractUniqueValueValidator extends ConstraintValidator
+{
+    /**
+     * @var EntityManagerInterface $entityManager
+     */
+    protected $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function validate($value, Constraint $constraint)
+    {
+        if ($value === null || $value == '') {
+            throw new UnexpectedTypeException('Value can not be empty.', 'string');
+        }
+
+        if ($this->checkValueExists($value)) {
+            $this->context->buildViolation($constraint->message)
+                ->atPath($this->getPath())
+                ->addViolation();
+        }
+    }
+
+    abstract protected function checkValueExists(string $value);
+
+    abstract protected function getPath(): string;
+}
---
+++ src/Validator/Constraints/UniqueValues/UniqueValue.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Validator\Constraints\UniqueValues;
+
+use Symfony\Component\Validator\Constraint;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+/**
+ * @Annotation
+ */
+class UniqueEmail extends Constraint
+{
+    /**
+     * @var string $entityClass
+     */
+    protected $entityClass;
+
+    public function __construct($entityClass, $options = null)
+    {
+        $this->entityClass = $entityClass;
+
+        parent::__construct($options);
+    }
+
+    public $message = 'Value is already in use.';
+
+    public function getEntityClass(): string
+    {
+        return $this->entityClass;
+    }
+}
