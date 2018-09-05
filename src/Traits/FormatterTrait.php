8dc9cfb5bf149edb3ca0df8558251443486bd747
Bohdan <batievbt@gmail.com>
2018-09-05 08:13:39 +0000

    Formatter trait

---
+++ src/Traits/FormatterTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Helpcrunch\Traits;
+
+trait FormatterTrait
+{
+    protected function convertUnderscoreToCamelCase(string $string): string
+    {
+        return lcfirst(implode('', array_map('ucfirst', explode('_', $string))));
+    }
+
+    protected function arrayKeysToCamelCase(array $data): array
+    {
+        foreach ($data as $key => $value) {
+            $camelCaseKey = $this->convertUnderscoreToCamelCase($key);
+            $data[$camelCaseKey] = $value;
+        }
+
+        return $data;
+    }
+}
