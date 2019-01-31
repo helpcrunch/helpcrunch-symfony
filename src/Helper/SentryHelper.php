2744d4f7369bc060ea1c17fdcd3ccc469b960be6
Pavel Prykhodko <mtk@fervallen.com>
2019-01-31 17:10:33 +0200

    Added Sentry helper (#19)

---
+++ src/Helper/SentryHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Helpcrunch\Helper;
+
+use \Raven_Client;
+
+class SentryHelper
+{
+    /**
+     * @var \Raven_Client
+     */
+    private static $ravenClient;
+
+    public static function install(string $ravenUrl = null): void
+    {
+        if (!$ravenUrl) {
+            if (!empty($_SERVER['RAVEN_URL'])) {
+                $ravenUrl = $_SERVER['RAVEN_URL'];
+            } else {
+                throw new \InvalidArgumentException('No raven url provided');
+            }
+        }
+
+        self::$ravenClient = new Raven_Client($ravenUrl);
+        self::$ravenClient->install($ravenUrl);
+    }
+
+    public static function log($message): void
+    {
+        if (!self::$ravenClient) {
+            return;
+        }
+
+        if (is_array($message) || is_object($message)) {
+            $message = json_encode($message);
+        }
+        self::$ravenClient->captureMessage($message, ['log'], ['level' => \Raven_Client::DEBUG]);
+    }
+}
