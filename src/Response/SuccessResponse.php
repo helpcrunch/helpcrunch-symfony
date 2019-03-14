c6c12063ccabd0b40addac4af69fca41e0eab9bc
Prykhodko Pavel <mtk@fervallen.com>
2019-03-14 18:08:35 +0200

    Fixed serializer naming strategry issue

--- src/Response/SuccessResponse.php
+++ src/Response/SuccessResponse.php
@@ -4,6 +4,7 @@ namespace Helpcrunch\Response;
 
 use Helpcrunch\Entity\HelpcrunchEntity;
 use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
+use JMS\Serializer\Naming\SerializedNameAnnotationStrategy;
 use JMS\Serializer\Serializer;
 use JMS\Serializer\SerializerBuilder;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -25,7 +26,11 @@ class SuccessResponse extends JsonResponse
     {
         /** @var Serializer $serializer */
         $serializer = SerializerBuilder::create()
-            ->setPropertyNamingStrategy(new IdenticalPropertyNamingStrategy())
+            ->setPropertyNamingStrategy(
+                new SerializedNameAnnotationStrategy(
+                    new IdenticalPropertyNamingStrategy()
+                )
+            )
             ->build();
 
         return $serializer->toArray($entity);
