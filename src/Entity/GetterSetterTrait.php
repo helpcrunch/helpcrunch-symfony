bd5b1d1f8fdbba231a05b7481177e8a8c4f7f79e
Prykhodko Pavel <mtk@fervallen.com>
2019-03-21 11:34:36 +0200

    Added support for getter & setter methods in entity

--- src/Entity/GetterSetterTrait.php
+++ src/Entity/GetterSetterTrait.php
@@ -10,7 +10,9 @@ trait GetterSetterTrait
      */
     public function __get($name)
     {
-        if ($this->checkPropertyExists($name)) {
+        if ($this->getterExists($name)) {
+            return $this->executeGetter($name);
+        } elseif ($this->checkPropertyExists($name)) {
             return $this->$name;
         }
 
@@ -24,7 +26,9 @@ trait GetterSetterTrait
      */
     public function __set($name, $value)
     {
-        if ($this->checkPropertyExists($name)) {
+        if ($this->setterExists($name)) {
+            $this->executeSetter($name, $value);
+        } elseif ($this->checkPropertyExists($name)) {
             $this->$name = $value;
         }
 
@@ -42,5 +46,39 @@ trait GetterSetterTrait
         }
 
         return true;
+    }
+
+    protected function getterExists(string $name): bool
+    {
+        return method_exists($this, $this->getMethodName('get', $name));
+    }
+
+    protected function setterExists(string $name): bool
+    {
+        return method_exists($this, $this->getMethodName('set', $name));
+    }
+
+    protected function executeGetter(string $name)
+    {
+        $methodName = $this->getMethodName('get', $name);
+
+        return $this->$methodName();
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return $this|null
+     */
+    protected function executeSetter(string $name, $value)
+    {
+        $methodName = $this->getMethodName('set', $name);
+
+        return $this->$methodName($value);
+    }
+
+    protected function getMethodName(string $methodType, string $name): string
+    {
+        return $methodType . strtoupper(substr($name, 0, 1)) . substr($name, 1);
     }
 }
