1b9dccfa4cc72f11cfe05b58580ae62871eec6d9
Prykhodko Pavel <mtk@fervallen.com>
2019-06-13 18:02:03 +0300

    Add additional serializing strategy

--- src/Response/SuccessResponse.php
+++ src/Response/SuccessResponse.php
@@ -2,6 +2,11 @@
 
 namespace Helpcrunch\Response;
 
+use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
+use JMS\Serializer\Naming\SerializedNameAnnotationStrategy;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SuccessResponse extends JsonResponse
@@ -17,6 +22,23 @@ class SuccessResponse extends JsonResponse
         }
         $responseData['success'] = true;
 
-        parent::__construct($responseData, $status);
+        parent::__construct($this->serialize($responseData), $status);
+    }
+
+    protected function serialize(array $responseData): array
+    {
+        /** @var Serializer $serializer */
+        $serializer = SerializerBuilder::create()
+            ->setPropertyNamingStrategy(
+                new SerializedNameAnnotationStrategy(
+                    new IdenticalPropertyNamingStrategy()
+                )
+            )
+            ->build();
+
+        $context = new SerializationContext();
+        $context->setSerializeNull(true);
+
+        return $serializer->toArray($responseData, $context);
     }
 }
