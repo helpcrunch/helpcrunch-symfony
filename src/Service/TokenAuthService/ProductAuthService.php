f723339fc90e20fb149e384f356126c1deaaa33d
Bohdan <batievbt@gmail.com>
2019-09-17 15:57:26 +0300

    HC-4606 Product auth

---
+++ src/Service/TokenAuthService/ProductAuthService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Helpcrunch\Service\TokenAuthService;
+
+use Helpcrunch\Service\AbstractTokenAuthService;
+
+class ProductAuthService extends AbstractTokenAuthService
+{
+    const DEVICE_TOKEN_LENGTH = 64;
+
+    /**
+     * @var int
+     */
+    private $productId;
+
+    public function isTokenValid(): bool
+    {
+        return ($this->productId == $this->getRedisService()->getData($this->getTokenKey()));
+    }
+
+    protected function generateToken(): string
+    {
+        return base64_encode(openssl_random_pseudo_bytes(self::DEVICE_TOKEN_LENGTH));
+    }
+
+    public function setProductId(int $productId): self
+    {
+        $this->productId = $productId;
+
+        return $this;
+    }
+}
