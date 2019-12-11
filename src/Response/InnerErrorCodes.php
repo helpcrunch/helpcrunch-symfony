b25dcc1b6c5d37cadd88b5687cddbd91bed21227
Vladyslav Syniavskyi <vladyslav.syniavskyi@helpcrunch.com>
2019-12-11 10:02:25 +0200

    HC-4790 New API docs for mobile devs | InnerErrorCodes to camelCase

--- src/Response/InnerErrorCodes.php
+++ src/Response/InnerErrorCodes.php
@@ -5,14 +5,14 @@ namespace Helpcrunch\Response;
 class InnerErrorCodes
 {
     const UNAUTHORIZED = 'unauthorized';
-    const ENTITY_NOT_FOUND = 'entity_not_found';
-    const ENTITY_ALREADY_EXIST = 'entity_already_exist';
-    const INVALID_ENTITY_ID = 'invalid_entity_id';
-    const INVALID_PARAMETER = 'invalid_parameter';
-    const MALFORMED_JSON = 'malformed_json';
-    const MISSING_PARAMETER = 'missing_parameter';
-    const PARENT_ENTITIES_MISMATCH = 'parent_entities_mismatch';
-    const POST_ENTITY_VALIDATION_FAILED = 'create_entity_fields_validation_failed';
-    const PUT_ENTITY_VALIDATION_FAILED = 'update_entity_fields_validation_failed';
-    const SERVER_ERROR = 'server_error';
+    const ENTITY_NOT_FOUND = 'entityNotFound';
+    const ENTITY_ALREADY_EXIST = 'entityAlreadyExist';
+    const INVALID_ENTITY_ID = 'invalidEntityId';
+    const INVALID_PARAMETER = 'invalidParameter';
+    const MALFORMED_JSON = 'malformedJson';
+    const MISSING_PARAMETER = 'missingParameter';
+    const PARENT_ENTITIES_MISMATCH = 'parentEntitiesMismatch';
+    const POST_ENTITY_VALIDATION_FAILED = 'createEntityFieldsValidationFailed';
+    const PUT_ENTITY_VALIDATION_FAILED = 'updateEntityFieldsValidationFailed';
+    const SERVER_ERROR = 'serverError';
 }
