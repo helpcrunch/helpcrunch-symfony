8d841e432ddcf13dfca00648240ed7a4de75dd7c
Vladyslav Syniavskyi <vladyslav.syniavskyi@helpcrunch.com>
2020-02-20 10:38:57 +0200

    HC-4790-stage2 | Timestamp fix

--- src/Service/EntityFieldsParserService.php
+++ src/Service/EntityFieldsParserService.php
@@ -7,7 +7,7 @@ use Helpcrunch\Entity\HelpcrunchEntity;
 
 class EntityFieldsParserService
 {
-    const DATETIME_FORMAT = 'Y-m-d H:i:s';
+    const DATETIME_FORMAT = 'U.v';
 
     public function parse(array $fields): array
     {
