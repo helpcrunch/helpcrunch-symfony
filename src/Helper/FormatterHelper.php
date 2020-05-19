c3436547cd81ade8d4ed91ff48e0f4be3b5ab13e
Bohdan Batiiev <batievbt@gmail.com>
2020-05-19 12:44:17 +0000

    Return null or string

--- src/Helper/FormatterHelper.php
+++ src/Helper/FormatterHelper.php
@@ -4,17 +4,17 @@ namespace Helpcrunch\Helper;
 
 class FormatterHelper
 {
-    public static function convertUnderscoreToCamelCase(string $string): string
+    public static function convertUnderscoreToCamelCase(string $string): ?string
     {
         return lcfirst(implode('', array_map('ucfirst', explode('_', $string))));
     }
 
-    public static function convertCamelCaseToUnderscore(string $string): string
+    public static function convertCamelCaseToUnderscore(string $string): ?string
     {
         return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $string));
     }
 
-    public static function clearString(string $string): string
+    public static function clearString(string $string): ?string
     {
         return preg_replace('/[^\p{L}\p{N}_@\.]+/u', ' ', $string);
     }
