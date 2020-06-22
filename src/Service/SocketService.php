24b5fffa93b4c8ed223b39fcb70bedafc86304b8
dmitry-pilipenko <dmitry@pilipenko.family>
2020-06-22 16:41:45 +0300

    HC-5721: Changed SocketService events transporter from ZeroMQ to Redis

--- src/Service/SocketService.php
+++ src/Service/SocketService.php
@@ -2,47 +2,33 @@
 
 namespace Helpcrunch\Service;
 
-use InvalidArgumentException;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use ZMQ;
-use ZMQContext;
-use ZMQSocket;
+use Predis\ClientInterface;
 
 class SocketService
 {
     /**
-     * @var ContainerInterface
+     * @var ClientInterface
      */
-    private $container;
+    protected $client;
 
     /**
-     * @var ZMQSocket
+     * SocketService constructor.
      */
-    private $socket;
-
-    /**
-     * @var bool
-     */
-    private $connected = false;
-
-    public function __construct(ContainerInterface $container)
+    public function __construct(ClientInterface $client)
     {
-        $this->container = $container;
-
-        $this->socket = (new ZMQContext())->getSocket(ZMQ::SOCKET_PUSH);
+        $this->client = $client;
     }
 
+    /**
+     * Sends data to redis pub/sub
+     */
     public function sendEvent(array $data): void
     {
-        $socketDSN = $this->container->getParameter('socket_connection');
-        if (!$socketDSN) {
-            throw new InvalidArgumentException('Missing socket configuration');
-        }
-        if (!$this->connected) {
-            $this->socket->connect($socketDSN);
-            $this->connected = true;
-        }
-
-        $this->socket->send(json_encode($data), ZMQ::MODE_DONTWAIT);
+        $this->client->publish(
+            $data['event_id'],
+            !empty($data['event_data']) ?
+                json_encode($data['event_data']) :
+                null
+        );
     }
 }
